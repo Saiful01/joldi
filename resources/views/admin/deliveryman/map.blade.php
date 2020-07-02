4310edb20822facb9f627f90cab40515acffe12b
memotiur <memotiur@gmail.com>
2020-07-03 04:52:56 +0600

    ..

--- resources/views/admin/deliveryman/map.blade.php
+++ resources/views/admin/deliveryman/map.blade.php
@@ -8,6 +8,7 @@
         #map {
             height: 100%;
         }
+
         /* Optional: Makes the sample page fill the window. */
         html, body {
             height: 100%;
@@ -33,58 +34,48 @@
 
     <div class="row">
         <div class="col-12">
-            <div id="map" style="margin:50px"></div>
-            <script>
-                var map;
-                function initMap() {
-                    map = new google.maps.Map(document.getElementById('map'), {
-                        zoom: 12,
-                        center: new google.maps.LatLng(23.8052091,90.3433764),
-                        mapTypeId: 'terrain'
-                    });
-​
-                    // Create a <script> tag and set the USGS URL as the source.
-                    var script = document.createElement('script');
-                    // This example uses a local copy of the GeoJSON stored at
-                    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-                    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-                    document.getElementsByTagName('head')[0].appendChild(script);
-                }
-​
-                // Loop through the results array and place a marker for each
-                // set of coordinates.
-                window.eqfeed_callback = function(results) {
-                    //for (var i = 0; i < results.features.length; i++) {
-
-                    var latLng = new google.maps.LatLng(23.8052091,90.3433764);
-                    var marker = new google.maps.Marker({
-                        position: latLng,
-                        map: map
-                    });
-
-                    //Next
-
-                    var latLng = new google.maps.LatLng(23.8052071,90.3433754);
-                    var marker = new google.maps.Marker({
-                        position: latLng,
-                        map: map
-                    });
-                    //Next
-                    //var coords = results.features[i].geometry.coordinates;
-                    var latLng = new google.maps.LatLng(23.7871595,90.3535339);
-                    var marker = new google.maps.Marker({
-                        position: latLng,
-                        map: map
-                    });
-                    // }
-                }
-            </script>
-            <script async defer
-                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEDWXvfmNADy9lDpov7EBTwSCL5GxL7KM&callback=initMap">
-            </script>
-
+            <div id="map" style="width: 100%; height: 400px;"></div>
 
         </div> <!-- end col -->
     </div> <!-- end row -->
 
+
+
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEDWXvfmNADy9lDpov7EBTwSCL5GxL7KM"></script>
+
+
+
+    <script type="text/javascript">
+        var locations = [
+            ['Bondi Beach', 23.7871595, 90.3535339],
+            ['Bondi Beach', 23.7872595, 90.4545339],
+            ['Bondi Beach', 23.4872595, 90.4545339],
+
+        ];
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: new google.maps.LatLng(23.92, 90.25),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+    </script>
 @endsection
